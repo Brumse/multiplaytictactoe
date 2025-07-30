44d860b72f1965660cd0b8488bf9d42a2ec2f2a0
andfroswe <andfro@protonmail.com>
2025-07-30 21:06:26 +0200

    Add Google Test and dummy test case

---
+++ cmake/GetGoogleTest.cmake
@@ -0,0 +1,9 @@
+include(FetchContent)
+FetchContent_Declare(
+        googletest
+        URL https://github.com/google/googletest/archive/52eb8108c5bdec04579160ae17225d66034bd723.zip
+)
+
+# For Windows: Prevent overriding the parent project's compiler/linker settings
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+FetchContent_MakeAvailable(googletest)
